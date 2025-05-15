884daac9aeb6711d650116212806899537ae4306
drxxmn <github@senteya.com>
2025-05-15 14:54:45 +0200

    inspo from hyprdots waybar and new keybinds

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Symlink config directories
+# Remove existing symlinks or directories
+rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
+rm -rf ~/.config/kitty
+rm -rf ~/.config/fish
+rm -f ~/.bashrc
+rm -f ~/.config/rofi
+
+# Create new symlinks
 ln -sf ~/dotfiles/.config/hypr ~/.config/hypr
 ln -sf ~/dotfiles/.config/waybar ~/.config/waybar
 ln -sf ~/dotfiles/.config/kitty ~/.config/kitty
 ln -sf ~/dotfiles/.config/fish ~/.config/fish
-
-# Symlink individual files
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.config/rofi ~/.config/rofi
 
-echo "Dotfiles installed and symlinked."
+
+echo "Symlinks created successfully."
