46f3273d23c6e463f8c8ab62063fb461c85f9803
drxxmn <github@senteya.com>
2025-05-16 12:02:36 +0200

    fixed rofi menu, height needed to be fixed true to not stretch

--- .config/misc/keybinds/keybinds.sh
+++ .config/misc/keybinds/keybinds.sh
@@ -6,4 +6,4 @@ keybinds="$HOME/dotfiles/.config/misc/keybinds/keybinds.txt"
 keybinds1=$(cat "$keybinds")
 
 # Usa o rofi para exibir as keybinds
-echo "$keybinds1" | rofi -dmenu -theme ~/dotfiles/.config/misc/keybinds/keybinds.rasi -p "Keybinds"
+echo "$keybinds1" | rofi -dmenu -theme ~/dotfiles/.config/misc/keybinds/keybinds.rasi -p "Keybinds" -i
